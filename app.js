b0c3bac89d7732f6c0ea4c9618c4d0410e9dc160
JH <wlgud5632@naver.com>
2024-11-01 12:13:47 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// alert("test");
+
+//클래스 panel들을 모두 선택해 상수 panels에 담기
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
